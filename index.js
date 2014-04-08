d49dab6a55fbf0dec4b87c0d5ce299206ea991a2
Remy Sharp <remy@leftlogic.com>
2014-04-08 12:03:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ var config = require('./config');
 
 var zmq = require('zmq');
 var socket = zmq.socket('pull');
+var Promise = require('rsvp').Promise;
 socket.connect(config.port);
 
 var dropbox = require('jsbin-dropbox-sync');
+
+function parse(string) {
+  return new Promise(function (resolve, reject) {
+    resolve(JSON.parse(string));
+  });
+}
+
+socket.on('message', function (buffer) {
+  parse(buffer.toString()).then(function (data) {
+    if (data.type === 'dropbox') {
+      dropbox.process(data); // whatevs...
+    }
+  }).catch(function () {
+    console.error('invalid message: ' + buffer.toString());
+  });
+});
+
 dropbox(socket, config);
